8fcf7d18a51de37c575303f38f0aea62c493ca70
id0ba <72280374+ID0BA@users.noreply.github.com>
2024-11-14 13:25:37 +0000

    built out login function.

--- ATM.py
+++ ATM.py
@@ -1,9 +1,10 @@
 import random
 import csv
+import time
 
 def start():
-    print("""Is this your first time using our bank? 
-             If it is please type 1 to create your account. 
+    print("""Is this your first time using AIQ bank? 
+             If it is please type 1 to create an account. 
              If not please type 2 to log in.
           """)
     while True:
@@ -22,10 +23,12 @@ def start():
             print("Invalid input. Please enter a number")
 
 def create():
-    print("Welcome to the bank account creation page")
+    print("Welcome to the AIQ bank account creation page")
 
     while True:
-        user_name = input("Enter your name: ") # username
+        user_name = input(""""Enter a username
+                          Please note only letters are allowed
+                        : """) # username
 
         if not user_name.isalpha():# check if username is string only, took this outta the try loop since its not a valueError? right? 
             print("Invalid username. Only letters are allowed. Please try again.")
@@ -35,7 +38,7 @@ def create():
             user_password = int(input("Enter a 4 digit PIN: ")) # convert password to interger
 
             if len(str(user_password)) !=4: # check if password length is longer than 4 digits, god why doesnt len work with intergers took me way to long to solve this -_-
-                print("Invalid username or password, please try again")
+                print("Invalid password, please try again")
             else:
                 
                 with open("users.csv", mode="a", newline="") as file: # creating file if it doesnt exist
@@ -46,16 +49,54 @@ def create():
 
                     writer.writerow([user_name, user_password]) # writing user data to file
                 
-                print("Account created successfully")
+                print("AIQ Bank account created successfully")
                 start()
                 break
 
         except ValueError:
-            print("Invalid input, please eneter a valid 4 digit PIN")
+            print("Invalid input, please enter a 4 digit PIN")
             continue # restart the loop
 
 def login():
-    print("placeholder")
+    print("Welcome to AIQ bank")
+
+    # setting max number of attempts allowed to prevent exploitation
+    max_attempts = 4 
+    attempts = 0
+
+    #checks how many attempts have been made
+    while attempts < max_attempts:
+        
+        Username=input("Please enter your username: ") #gathering username and pin
+        Userpin=input("Please enter your PIN: ")
+
+        #opening csv and comparing stored creds against user inputs
+        with open ("users.csv", mode="r") as file:
+            reader=csv.DictReader(file)
+
+            for row in reader:
+                if row["Usernames"] == Username:
+                    if row['Passwords'] == Userpin:
+                        print("Identity verified, login successful.")
+                        print("Moving to menu, please wait")
+                        time.sleep(5)
+                        menu()
+                    else:
+                        print
+                        print("incorrect Pin please try again")
+                        attempts += 1
+                        break
+                else:
+                    print("incorrect username please try again")
+                    attempts += 1
+                    break
+        if attempts < max_attempts:
+            print(f"you have {max_attempts - attempts} attempts left")
+        else:
+            print("Too many failed attempts. Please try again later.")
+            print("AIQ bank will be locked for 60 seconds")
+            time.sleep(60)
+            return False
 
 def menu():
     int(input("""welcome to the menu.
@@ -68,7 +109,12 @@ def menu():
         pin()
 
 def pin():
-    print("placeholder")
+    print("""
+          Welcome to the Pin change screen
+          For security reasons we require you to re-enter your PIN""")
+    
+    pin = int(input("Enter your current PIN: "))
+
 
 def balance():
     print("Placeholder")
@@ -94,5 +140,6 @@ if __name__ == "__main__":
     print(f"Loading... {random4}%")
     print(f"Loading.. {random5}%")
     print("""Loading complete,
-            Welcome.""")
+            AIQ Bank, 
+          a service you can trust.""")
     start()
